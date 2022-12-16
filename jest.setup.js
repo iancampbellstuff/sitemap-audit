a3bc0783b4f86be12d0a7219425913faf6c7a05f
Ian Campbell <10663545+iancampbellstuff@users.noreply.github.com>
2022-12-16 14:19:52 -0500

    moving envUtils getEnvironmentVariables jest mock from sitemapUtils unit tests to global jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,12 @@
 const nock = require('nock');
 
+jest.mock('./src/utils/envUtils', () => ({
+    getEnvironmentVariables: () => ({
+        SITEMAP_URL: 'https://www.google.com/sitemap.xml',
+        URL_COUNT_THRESHOLD: 100
+    })
+}));
+
 beforeAll(() => {
     nock.disableNetConnect();
 });
