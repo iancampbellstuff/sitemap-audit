43027177687fb329b590f37ed60af08df91b477c
Ian Campbell <10663545+iancampbellstuff@users.noreply.github.com>
2022-12-23 14:45:33 -0500

    DRY refactoring in axiosUtils using new enum AxiosInterceptorType and helper function toggleErrors

--- src/utils/axiosUtils.ts
+++ src/utils/axiosUtils.ts
@@ -1,17 +1,37 @@
 import axios from 'axios';
 
+enum AxiosInterceptorType {
+    REQUEST,
+    RESPONSE
+}
+
+/**
+ * This function toggles errors on or off depending on the given boolean flag, for a given axios interceptor type.
+ *
+ * @param enable the boolean flag used to toggle request errors on or off
+ * @param axiosInterceptorType the axios interceptor type to toggle errors for, defaulting to requests
+ */
+const toggleErrors = (enable?: boolean, axiosInterceptorType: AxiosInterceptorType = AxiosInterceptorType.REQUEST) => {
+    const onFulfilledHandler = (config) => {
+        // Do something before request is sent
+        return config;
+    };
+    const onRejectedHandler = async (error) => {
+        return enable ? await Promise.reject(error) : await Promise.resolve(error);
+    };
+    const axiosInterceptorManager = axiosInterceptorType === AxiosInterceptorType.RESPONSE
+        ? axios.interceptors.response
+        : axios.interceptors.request;
+    axiosInterceptorManager.use(onFulfilledHandler, onRejectedHandler);
+};
+
 /**
  * This function toggles request errors on or off, depending on the given boolean flag.
  *
  * @param enable the boolean flag used to toggle request errors on or off
  */
 export const toggleRequestErrors = (enable?: boolean) => {
-    axios.interceptors.request.use((config) => {
-        // Do something before request is sent
-        return config;
-    }, async (error) => {
-        return enable ? await Promise.reject(error) : await Promise.resolve(error);
-    });
+    toggleErrors(enable, AxiosInterceptorType.REQUEST);
 };
 
 /**
@@ -20,12 +40,7 @@ export const toggleRequestErrors = (enable?: boolean) => {
  * @param enable the boolean flag used to toggle response errors on or off
  */
 export const toggleResponseErrors = (enable?: boolean) => {
-    axios.interceptors.response.use((response) => {
-        // Do something with response data
-        return response;
-    }, async (error) => {
-        return enable ? await Promise.reject(error) : await Promise.resolve(error);
-    });
+    toggleErrors(enable, AxiosInterceptorType.RESPONSE);
 };
 
 /**
